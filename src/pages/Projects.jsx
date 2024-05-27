c39bc7377f6d310f30c926d513590dc5cd59f108
Chang'ach <126554394+changach-3000@users.noreply.github.com>
2024-05-27 22:55:00 +0300

    create cards for the projects and sample reports

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+
+function Projects() {
+  return (
+    <div class="flex flex-wrap justify-center items-center min-h-screen p-8 bg-gray-100">
+  <div class="max-w-md w-full m-4">
+    <div class="relative overflow-hidden rounded-lg shadow-lg group">
+      <a href="#">
+        <img class="object-cover w-full h-full" src="bg.avif" alt="Noteworthy technology acquisitions 2021" />
+      </a>
+      <div class="absolute inset-0 bg-black bg-opacity-0 flex items-center justify-center opacity-0 transition-opacity duration-300 group-hover:bg-opacity-75 group-hover:opacity-100">
+        <div class="text-center text-white p-5">
+          <a href="#">
+            <h5 class="text-2xl font-bold tracking-tight">Noteworthy technology acquisitions 2021</h5>
+          </a>
+          <p class="font-normal">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+          <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Read more
+            <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+            </svg>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="max-w-md w-full m-4">
+    <div class="relative overflow-hidden rounded-lg shadow-lg group">
+      <a href="#">
+        <img class="object-cover w-full h-full" src="bg.avif" alt="Noteworthy technology acquisitions 2021" />
+      </a>
+      <div class="absolute inset-0 bg-black bg-opacity-0 flex items-center justify-center opacity-0 transition-opacity duration-300 group-hover:bg-opacity-75 group-hover:opacity-100">
+        <div class="text-center text-white p-5">
+          <a href="#">
+            <h5 class="text-2xl font-bold tracking-tight">Noteworthy technology acquisitions 2021</h5>
+          </a>
+          <p class="font-normal">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+          <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Read more
+            <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+            </svg>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="max-w-md w-full m-4">
+    <div class="relative overflow-hidden rounded-lg shadow-lg group">
+      <a href="#">
+        <img class="object-cover w-full h-full" src="bg.avif" alt="Noteworthy technology acquisitions 2021" />
+      </a>
+      <div class="absolute inset-0 bg-black bg-opacity-0 flex items-center justify-center opacity-0 transition-opacity duration-300 group-hover:bg-opacity-75 group-hover:opacity-100">
+        <div class="text-center text-white p-5">
+          <a href="#">
+            <h5 class="text-2xl font-bold tracking-tight">Noteworthy technology acquisitions 2021</h5>
+          </a>
+          <p class="font-normal">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+          <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Read more
+            <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+            </svg>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+  )
+}
+
+export default Projects
