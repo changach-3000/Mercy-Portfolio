9398e931b170645f1971cdd4599ab436df4bc962
Chang'ach <126554394+changach-3000@users.noreply.github.com>
2024-05-27 22:56:48 +0300

    create keystrokes and animations for transitions and animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'float-in': 'float-in 10s ease-out forwards',
+        'float-in-right': 'float-in-right 5s ease-out forwards',
+    },
+    keyframes: {
+      'float-in': {
+        '0%': { transform: 'translateY(20px)', opacity: '0' },
+        '100%': { transform: 'translateY(0)', opacity: '1' },
+        },
+        'float-in-right': {
+            '0%': { transform: 'translateX(20px)', opacity: '0' },
+            '100%': { transform: 'translateX(0)', opacity: '1' },
+          }
+    },
+    },
   },
   plugins: [],
 }
