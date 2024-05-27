d6ee36e27590a2bf476bdea537f4f21a13a9b826
Chang'ach <126554394+changach-3000@users.noreply.github.com>
2024-05-27 22:54:11 +0300

    add experience page for work experience

---
+++ src/pages/Experience.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Timeline from '../components/Timeline'
+
+function Experience() {
+  return (
+    <div className='bg-black'>
+    <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-pink-900" id='header'>
+          EXPERIENCE
+        </h2>
+        <p class="mb-5 font-light text-pink-900 text-center sm:text-xl dark:text-gray-400">
+         This is a recall and and retrieval exercise with some key takeaways from the sessions
+        </p>
+        <Timeline />
+        </div>
+  )
+}
+
+export default Experience
